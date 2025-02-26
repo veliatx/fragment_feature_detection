f7686811f242f23da01a1f0e9ee96f53574cf082
ledwmp <mitch.ledwith@gmail.com>
2025-02-26 16:28:43 +0000

    Swap defaults and add extra feature_output section to config.

--- fragment_feature_detection/config.py
+++ fragment_feature_detection/config.py
@@ -1,12 +1,12 @@
-import logging
-from pathlib import Path 
-from typing import * 
-
 import configparser
+import logging
+from pathlib import Path
+from typing import Any, Union
+
 
 def format_logger(logger: logging.Logger, level: int = logging.INFO) -> None:
     """Configure basic formatting for a logger.
-    
+
     Args:
         logger: Logger instance to configure
     """
@@ -15,8 +15,9 @@ def format_logger(logger: logging.Logger, level: int = logging.INFO) -> None:
     )
     for handler in logger.handlers:
         handler.setFormatter(formatter)
-    
+
     logger.setLevel(level)
+
 
 class AttributeDict(dict):
     """Dictionary with attribute-style access"""
@@ -40,8 +41,8 @@ class AttributeDict(dict):
 
 class Config:
     random_seed = 42
-    tqdm_enabled = True
-    lazy = False
+    tqdm_enabled = False
+    lazy = True
     downcast_intensities = True
 
     h5 = AttributeDict(
@@ -57,38 +58,38 @@ class Config:
             "mz_low": 200.0,
             "mz_high": 2000.0,
             "steps": 2,
-            "tolerance": 60.0 / 1e6,
+            "tolerance": 20.0 / 1e6,
         }
     )
 
     tuning = AttributeDict(
         **{
+            "run_tune": True,
             "optuna": True,
             "window_sampling_n": 50,
             "window_sampling_fraction": 0.0125,
             "exclude_scan_window_edges": 10,
             "optuna_hyperparameter_grid": {
-                'l1_ratio': (0.1, 0.9),
-                'alpha_W': (0.0001, 0.1, 'log'),
-                'alpha_H': (0.0001, 0.1, 'log'),
+                "l1_ratio": (0.1, 0.9),
+                "alpha_W": (0.0001, 0.1, "log"),
+                "alpha_H": (0.0001, 0.1, "log"),
             },
             "hyperparameter_grid": {
-                'alpha_W': [0.0001, 0.0005, 0.001, 0.005, 0.01],
-                'alpha_H': [0.0001, 0.0005, 0.001, 0.005, 0.01],
-                'l1_ratio': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9],
+                "alpha_W": [0.0001, 0.0005, 0.001, 0.005, 0.01],
+                "alpha_H": [0.0001, 0.0005, 0.001, 0.005, 0.01],
+                "l1_ratio": [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9],
             },
             "objective_params": [
-                'test_reconstruction_errors',
-                'neglog_ratio_train_test_reconstruction_error',
-                'sample_orthogonality',
-                'weight_orthogonality',
-                'fraction_window_component',
+                "test_reconstruction_errors",
+                "neglog_ratio_train_test_reconstruction_error",
+                "sample_orthogonality",
+                "weight_orthogonality",
+                "fraction_window_component",
             ],
             "n_iter": 500,
-            "n_jobs": 4,
             "n_components": 20,
             "components_in_window": 8.0,
-            "component_sigma": 3.0,
+            "component_sigma": 2.0,  # In units of scans (95% of peak density falls within 8 scans.)
             "n_splits": 3,
             "test_fraction": 0.2,
             "splitter_type": "Mask",
@@ -130,9 +131,18 @@ class Config:
 
     feature_matching = AttributeDict(
         **{
+            "match_ms1": True,
             "alpha": 0.1,
             "c_cutoff": 0.025,
             "extend_w_fraction": 0.5,
+        }
+    )
+
+    feature_output = AttributeDict(
+        **{
+            "top_n_rescaled_peaks": 200,  # Number of peaks to report for weight/reweighted spectra.
+            "matching_coefficient_threshold": 0.1,  # Minimum reporting threshold.
+            "top_n_ms1_matches": 4,  # Number of matches to report before ties.
         }
     )
 
@@ -155,19 +165,19 @@ class Config:
     @classmethod
     def from_ini(cls, ini_path: Union[str, Path]) -> "Config":
         """Load configuration from an INI file.
-        
+
         Args:
             ini_path (str): Path to the INI file
-            
+
         Returns:
             Config: New config instance with values from INI file
         """
         config = configparser.ConfigParser()
         config.optionxform = str
         config.read(ini_path)
-        
+
         instance = cls()
-        
+
         for section in config.sections():
             if hasattr(instance, section):
                 section_dict = getattr(instance, section)
@@ -176,7 +186,7 @@ class Config:
                     # Convert string values to appropriate types
                     try:
                         typed_value = eval(value)
-                    except:
+                    except ValueError:
                         typed_value = value
                     section_dict[key] = typed_value
             else:
@@ -184,36 +194,41 @@ class Config:
                 for key, value in config[section].items():
                     try:
                         typed_value = eval(value)
-                    except:
+                    except ValueError:
                         typed_value = value
                     setattr(instance, key, typed_value)
-                    
+
         return instance
 
     def to_ini(self, ini_path: Union[Path, str]) -> None:
         """Save current configuration to an INI file.
-        
+
         Args:
             ini_path (str): Path where to save the INI file
         """
         config = configparser.ConfigParser()
-        
+
         attributes = dict(**vars(self.__class__))
         attributes.update(vars(self))
-        
-        config['general'] = {}
+
+        config["general"] = {}
         for key, value in attributes.items():
-            if not key.startswith('_') and not isinstance(value, classmethod) and not callable(value):
+            if (
+                not key.startswith("_")
+                and not isinstance(value, classmethod)
+                and not callable(value)
+            ):
                 if isinstance(value, AttributeDict):
                     config[key] = {}
                     for k, v in value.items():
                         config[key][k] = repr(v)
                 else:
-                    config['general'][key] = repr(value)
-                            
-        with open(ini_path, 'w') as f:
+                    config["general"][key] = repr(value)
+
+        with open(ini_path, "w") as f:
             config.write(f)
 
 
 class Constants:
     isotope_mu = 1.008
+    proton_mu = 1.0073
